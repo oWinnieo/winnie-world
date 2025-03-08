a1bc2545e9d2e6c8041d05c83ccae76f09f3f495
oWinnieo <48428659+oWinnieo@users.noreply.github.com>
2025-03-08 23:57:53 +0800

    Submit 250308 11.57pm

--- my-app/pages/api/auth/[...nextauth].js
+++ my-app/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { userInfoHandler } from './userInfoHandler'
+import { userInfoHandler, userCheckedHandler } from './userInfoHandler'
 
 export const authOptions = {
   providers: [
