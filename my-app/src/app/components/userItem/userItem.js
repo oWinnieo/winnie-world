fbb88848f6ea837b235921d19665a1606c4fb28a
oWinnieo <48428659+oWinnieo@users.noreply.github.com>
2025-03-09 00:01:51 +0800

    Submit 250309 12.01am

--- my-app/src/app/components/userItem/userItem.js
+++ my-app/src/app/components/userItem/userItem.js
@@ -2,7 +2,7 @@ export const UserItem = ({ data }) => {
     return (
         <>
             <p>name: {data.name}</p>
-            <p>id: {data.id}</p>
+            <p>userId: {data.userId}</p>
             <p>_id: {data._id}</p>
             <p>image: {data.image}</p>
             <p>email: {data.email}</p>
