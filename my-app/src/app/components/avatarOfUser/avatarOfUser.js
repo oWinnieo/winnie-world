5fcdec75c4ea2ede346770632432a52d226c002e
oWinnieo <48428659+oWinnieo@users.noreply.github.com>
2025-03-15 00:06:39 +0800

    Submit 250315 12.06am

--- my-app/src/app/components/avatarOfUser/avatarOfUser.js
+++ my-app/src/app/components/avatarOfUser/avatarOfUser.js
@@ -1,12 +1,8 @@
 'user client'
-// import { useSession } from "next-auth/react"; // wtest auth
 import { Smile } from 'lucide-react'
-import { Image } from 'next/image'
 import './avatarOfUser.scss'
 export const AvatarOfUser = ({ srcImage }) => {
-    // const { data: session } = useSession(); // wtest
-    console.log('srcImage', srcImage)
-    return (srcImage ? <Image className="auth-avatar" src={srcImage} alt="User Avatar" /> : <Smile />)
+    return srcImage ? <img className="auth-avatar" src={srcImage} alt="User Avatar" /> : <Smile />
 }
 
 
